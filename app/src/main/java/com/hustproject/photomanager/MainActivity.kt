7b738ad334be2b5b127d9fd006918ff9269ec3dc
ROROBINBIN <464214931@qq.com>
2018-07-04 10:27:24 +0800

    add bottomnavigationbar selecting events

--- app/src/main/java/com/hustproject/photomanager/MainActivity.kt
+++ app/src/main/java/com/hustproject/photomanager/MainActivity.kt
@@ -1,14 +1,59 @@
 package com.hustproject.photomanager
 
+import android.content.ClipData
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.view.menu.MenuView
+import android.view.MenuItem
+import android.view.View
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var tab: BottomNavigationView
+    private lateinit var title:TextView;
+
+    private fun loadAllPic() {
+        title.setText(R.string.allPic)
+    }
+
+    private fun loadPhoto() {
+        title.setText(R.string.photo)
+    }
+
+    private fun loadTrash() {
+        title.setText(R.string.trash)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        title = findViewById(R.id.titleView)
+        tab = findViewById(R.id.BottomBar)
+
+       val listener = BottomNavigationView.OnNavigationItemSelectedListener {item:MenuItem ->
+           when(item.itemId) {
+                R.id.allPicView -> {
+                    loadAllPic()
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.photoView -> {
+                    loadPhoto()
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.trashView -> {
+                    loadTrash()
+                    return@OnNavigationItemSelectedListener true
+                }
+               else -> return@OnNavigationItemSelectedListener false
+           }
+        }
+        tab.setOnNavigationItemSelectedListener(listener)
+
+        tab.selectedItemId = R.id.photoView
 
         // Example of a call to a native method
         //sample_text.text = stringFromJNI()
